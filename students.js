bf582742da5d8beb33bd315fc7cc440eb42ed360
Joseph Francis Re <91065433+JosephFrancisRe@users.noreply.github.com>
2026-05-13 16:54:05 -0400

    Update student project data in students.js

--- students.js
+++ students.js
@@ -1,17 +1,6 @@
 /*
-  GOGA project data.
-
-  Keep student names as first name + last initial.
-
-  Required fields:
-  studentDisplayName: "Maria R."
-  projectTitle: "Space Dodger" or "" if unknown
-  projectType: "Game" or "Website"
-  projectLink: CodeHS share/embed link or student website link
-  featured: true or false
-
-  Optional:
-  year: "2026", "2027", etc.
+  Auto-generated from Google Classroom.
+  You can edit featured: true/false manually after export.
 */
 
 window.GOGA_CONFIG = {
@@ -22,59 +11,275 @@ window.GOGA_CONFIG = {
 
 window.GOGA_PROJECTS = [
   {
-    studentDisplayName: "Example A.",
-    projectTitle: "Maze Runner",
-    projectType: "Game",
-    projectLink: "https://codehs.com/",
-    featured: true,
-    year: "2026"
+    "studentDisplayName": "ERIC L.",
+    "projectTitle": "CodeHS",
+    "projectType": "Website",
+    "projectLink": "https://codehs.com/sandbox/ericlangerman/pizzaandgelato",
+    "featured": true,
+    "year": "2026"
   },
   {
-    studentDisplayName: "Example B.",
-    projectTitle: "Personal Portfolio",
-    projectType: "Website",
-    projectLink: "https://codehs.com/",
-    featured: true,
-    year: "2026"
+    "studentDisplayName": "LEAROY L.",
+    "projectTitle": "",
+    "projectType": "Website",
+    "projectLink": "https://pizza-pizza-16098616.codehs.me/contacts.html",
+    "featured": true,
+    "year": "2026"
   },
   {
-    studentDisplayName: "Example C.",
-    projectTitle: "Space Dodger",
-    projectType: "Game",
-    projectLink: "https://codehs.com/",
-    featured: true,
-    year: "2026"
+    "studentDisplayName": "JAYDEN D.",
+    "projectTitle": "CodeHS",
+    "projectType": "Website",
+    "projectLink": "https://codehs.com/sandbox/bruhjrtr09/zoo-website-5",
+    "featured": true,
+    "year": "2026"
   },
   {
-    studentDisplayName: "Example D.",
-    projectTitle: "",
-    projectType: "Game",
-    projectLink: "https://codehs.com/",
-    featured: false,
-    year: "2026"
+    "studentDisplayName": "GREGORY K.",
+    "projectTitle": "CodeHS",
+    "projectType": "Website",
+    "projectLink": "https://codehs.com/sandbox/gregorykreutter/greg-gets-it",
+    "featured": true,
+    "year": "2026"
   },
   {
-    studentDisplayName: "Example E.",
-    projectTitle: "",
-    projectType: "Website",
-    projectLink: "https://codehs.com/",
-    featured: false,
-    year: "2026"
+    "studentDisplayName": "SELINA Z.",
+    "projectTitle": "CodeHS",
+    "projectType": "Website",
+    "projectLink": "https://codehs.com/sandbox/cryingravel/webproject",
+    "featured": true,
+    "year": "2026"
   },
   {
-    studentDisplayName: "Example F.",
-    projectTitle: "Interactive Story",
-    projectType: "Game",
-    projectLink: "https://codehs.com/",
-    featured: false,
-    year: "2026"
+    "studentDisplayName": "CYRUS C.",
+    "projectTitle": "CodeHS",
+    "projectType": "Website",
+    "projectLink": "https://codehs.com/sandbox/guysplss/website-project",
+    "featured": true,
+    "year": "2026"
   },
   {
-    studentDisplayName: "Example G.",
-    projectTitle: "Creative Website",
-    projectType: "Website",
-    projectLink: "https://codehs.com/",
-    featured: false,
-    year: "2026"
+    "studentDisplayName": "XAVIER G.",
+    "projectTitle": "CodeHS",
+    "projectType": "Website",
+    "projectLink": "https://codehs.com/sandbox/xavierjg/wacky-pizza",
+    "featured": true,
+    "year": "2026"
+  },
+  {
+    "studentDisplayName": "BENJAMIN S.",
+    "projectTitle": "GitHub - BenStenglein/Staticsite: Static website project for SE 11 · GitHub",
+    "projectType": "Website",
+    "projectLink": "https://github.com/BenStenglein/Staticsite",
+    "featured": true,
+    "year": "2026"
+  },
+  {
+    "studentDisplayName": "BENJAMIN S.",
+    "projectTitle": "Re's Pizza",
+    "projectType": "Website",
+    "projectLink": "http://benstenglein.github.io/Staticsite/",
+    "featured": true,
+    "year": "2026"
+  },
+  {
+    "studentDisplayName": "ALLISON Q.",
+    "projectTitle": "CodeHS",
+    "projectType": "Website",
+    "projectLink": "https://codehs.com/sandbox/allis0nquit0/marios-pizzeria",
+    "featured": true,
+    "year": "2026"
+  },
+  {
+    "studentDisplayName": "DECLAN O.",
+    "projectTitle": "CodeHS",
+    "projectType": "Website",
+    "projectLink": "https://codehs.com/sandbox/declano518/website-project-2",
+    "featured": true,
+    "year": "2026"
+  },
+  {
+    "studentDisplayName": "RIO H.",
+    "projectTitle": "CodeHS",
+    "projectType": "Website",
+    "projectLink": "https://codehs.com/sandbox/riohayesdiaz/pizza-website",
+    "featured": true,
+    "year": "2026"
+  },
+  {
+    "studentDisplayName": "DANTE V.",
+    "projectTitle": "CodeHS",
+    "projectType": "Website",
+    "projectLink": "https://codehs.com/sandbox/dantevelazquez/video-game-and-movie-review-website",
+    "featured": true,
+    "year": "2026"
+  },
+  {
+    "studentDisplayName": "ISAAC G.",
+    "projectTitle": "CodeHS",
+    "projectType": "Website",
+    "projectLink": "https://codehs.com/sandbox/isaacgalindo/zoo-thingy",
+    "featured": true,
+    "year": "2026"
+  },
+  {
+    "studentDisplayName": "HUNTER D.",
+    "projectTitle": "",
+    "projectType": "Website",
+    "projectLink": "https://codehs.com/sandbox/hunterdavis/harlem-zoo",
+    "featured": true,
+    "year": "2026"
+  },
+  {
+    "studentDisplayName": "EDDY R.",
+    "projectTitle": "Zoo/Animal Conservatory | CodeHS",
+    "projectType": "Website",
+    "projectLink": "https://codehs.com/sandbox/eddyrodriguez/zooanimal-conservatory",
+    "featured": true,
+    "year": "2026"
+  },
+  {
+    "studentDisplayName": "DECLAN F.",
+    "projectTitle": "CodeHS",
+    "projectType": "Website",
+    "projectLink": "https://codehs.com/sandbox/aaaxolotl/walter-wotlamb-pizza",
+    "featured": true,
+    "year": "2026"
+  },
+  {
+    "studentDisplayName": "DECLAN F.",
+    "projectTitle": "",
+    "projectType": "Website",
+    "projectLink": "https://walter-wotlamb-pizza-16057047.codehs.me/index.html",
+    "featured": true,
+    "year": "2026"
+  },
+  {
+    "studentDisplayName": "JACOB P.",
+    "projectTitle": "index",
+    "projectType": "Website",
+    "projectLink": "https://jacobdpete-atschool.github.io/pizzeria-project/",
+    "featured": true,
+    "year": "2026"
+  },
+  {
+    "studentDisplayName": "JACOB P.",
+    "projectTitle": "GitHub - jacobdpete-atschool/pizzeria-project: pizzeria · GitHub",
+    "projectType": "Website",
+    "projectLink": "https://github.com/jacobdpete-atschool/pizzeria-project",
+    "featured": true,
+    "year": "2026"
+  },
+  {
+    "studentDisplayName": "AITOR Q.",
+    "projectTitle": "CodeHS",
+    "projectType": "Website",
+    "projectLink": "https://codehs.com/sandbox/aitorquito57/cat-pizzeria",
+    "featured": true,
+    "year": "2026"
+  },
+  {
+    "studentDisplayName": "AMIR V.",
+    "projectTitle": "CodeHS",
+    "projectType": "Website",
+    "projectLink": "https://codehs.com/sandbox/amirviz1/html",
+    "featured": true,
+    "year": "2026"
+  },
+  {
+    "studentDisplayName": "JORDAN W.",
+    "projectTitle": "Lovecraftian creature excebition | CodeHS",
+    "projectType": "Website",
+    "projectLink": "https://codehs.com/sandbox/comradejjw/lovecraftian-creature-excebition-1",
+    "featured": true,
+    "year": "2026"
+  },
+  {
+    "studentDisplayName": "ZAMIR K.",
+    "projectTitle": "GitHub - ZamirKarimov/Zoo: Github Repo for the Software Engineering Project · GitHub",
+    "projectType": "Website",
+    "projectLink": "https://github.com/ZamirKarimov/Zoo",
+    "featured": true,
+    "year": "2026"
+  },
+  {
+    "studentDisplayName": "HENRY A.",
+    "projectTitle": "CodeHS",
+    "projectType": "Website",
+    "projectLink": "https://codehs.com/sandbox/henrya2027/units-1-3-project",
+    "featured": true,
+    "year": "2026"
+  },
+  {
+    "studentDisplayName": "ELLIS A.",
+    "projectTitle": "Better Pizza Project",
+    "projectType": "Website",
+    "projectLink": "https://pizzaproject.oneapp.dev/",
+    "featured": true,
+    "year": "2026"
+  },
+  {
+    "studentDisplayName": "KOBE B.",
+    "projectTitle": "CodeHS",
+    "projectType": "Website",
+    "projectLink": "https://codehs.com/sandbox/kobebuezo/ice-cream-project",
+    "featured": true,
+    "year": "2026"
+  },
+  {
+    "studentDisplayName": "ROMULO R.",
+    "projectTitle": "",
+    "projectType": "Website",
+    "projectLink": "https://romulos-pizzaria-16065955.codehs.me/",
+    "featured": true,
+    "year": "2026"
+  },
+  {
+    "studentDisplayName": "MATEO M.",
+    "projectTitle": "Log In | CodeHS",
+    "projectType": "Website",
+    "projectLink": "https://codehs.com/student/5845951/section/778750/assignment/193732653?filepath=index.html",
+    "featured": true,
+    "year": "2026"
+  },
+  {
+    "studentDisplayName": "Yasin Y.",
+    "projectTitle": "GitHub - yasinyenigun-netizen/ubiquitous-guide: schoolproject · GitHub",
+    "projectType": "Website",
+    "projectLink": "https://github.com/yasinyenigun-netizen/ubiquitous-guide",
+    "featured": true,
+    "year": "2026"
+  },
+  {
+    "studentDisplayName": "Yasin Y.",
+    "projectTitle": "Armored Core VI: Fires of Rubicon",
+    "projectType": "Website",
+    "projectLink": "https://yasinyenigun-netizen.github.io/ubiquitous-guide/",
+    "featured": true,
+    "year": "2026"
+  },
+  {
+    "studentDisplayName": "Nick B.",
+    "projectTitle": "CodeHS",
+    "projectType": "Website",
+    "projectLink": "https://codehs.com/sandbox/noxfloret/restaran",
+    "featured": true,
+    "year": "2026"
+  },
+  {
+    "studentDisplayName": "JOSIAH R.",
+    "projectTitle": "CodeHS",
+    "projectType": "Website",
+    "projectLink": "https://codehs.com/sandbox/mechobliterate/mechobliteratecodehscom",
+    "featured": true,
+    "year": "2026"
+  },
+  {
+    "studentDisplayName": "Robert G.",
+    "projectTitle": "",
+    "projectType": "Website",
+    "projectLink": "https://codehs.com/sandbox/robert58/media-blog-1",
+    "featured": true,
+    "year": "2026"
   }
 ];
